e0a9932ff327b1c18a1660e3445a874a1cefcf44
Ponlapat Sanguansirikul <ppbasleng@gmail.com>
2023-08-26 14:39:39 +0700

    update assets position to public

--- src/components/Transcript.jsx
+++ src/components/Transcript.jsx
@@ -3,7 +3,7 @@ function Transcript() {
         <div className="flex flex-col justify-center items-center w-full">
             <h3 className="text-md font-bold mb-5">Transcript</h3>
             {/* svg from src/assets/ponlapat_transcript.svg */}
-            <img src="src/assets/ponlapat_transcript.svg" alt="transcript" className="h-full object-contain object-center" />
+            <img src="/ponlapat_transcript.svg" alt="transcript" className="h-full object-contain object-center" />
         </div>
     );
 }
